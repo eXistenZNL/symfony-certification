bdcd75e38e384b0608cb14594c598f09a6bda65d
Nico Stapelbroek <nstapelbroek@gmail.com>
2016-04-04 23:53:04 +0200

    DI: Finish the implementation of a Dice Service

--- src/AppBundle/Service/SixEyedDice.php
+++ src/AppBundle/Service/SixEyedDice.php
@@ -4,18 +4,28 @@ namespace AppBundle\Service;
 
 use AppBundle\Exception\IllegalArgumentException;
 
-class SixEyedDice implements DiceInterface {
+class SixEyedDice implements DiceInterface
+{
 
     /**
-     * D6Dice constructor. Fills the dice with the 6 values
+     * @var array
+     */
+    protected $values;
+
+    /**
+     * D6Dice constructor, fills the dice with the 6 values
+     *
      * @param array $values
+     *
      * @throws IllegalArgumentException
      */
-    public function __construct(array $values = array(1,2,3,4,5,6))
+    public function __construct(array $values = array(1, 2, 3, 4, 5, 6))
     {
-        if(count($values) != 6) {
+        if (count($values) != 6) {
             throw new IllegalArgumentException('A 6 sided dice must contain all 6 values');
         }
+
+        $this->values = $values;
     }
 
     /**
@@ -25,6 +35,6 @@ class SixEyedDice implements DiceInterface {
      */
     public function roll()
     {
-        // TODO: Implement roll() method.
+        return array_rand($this->values);
     }
 }
