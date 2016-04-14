4a61ed3e741d4cf237da5d79073d112d29995c70
Nico Stapelbroek <nstapelbroek@enrise.com>
2016-04-14 16:29:37 +0200

    DI: Add services declarations in XML and YML

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -10,6 +10,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 
 /**
@@ -22,21 +23,32 @@ class AppExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        // only load default services and parameters once  disabled because it did not work for me
-//        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-//        $loader->load('services.xml');
+        $strategies = array('xml', 'php', 'yml');
+        $strategyKey = rand(0, count($strategies));
 
-        $sixDiceDefinition = new Definition(SixSidedDice::class);
-        $sixDiceDefinition->addTag('app.rollable');
-        $container->setDefinition('app.dice.six', $sixDiceDefinition);
+        switch ($strategies[$strategyKey]) {
+            case "xml":
+                $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+                $loader->load('services.xml');
+                break;
+            case "php":
+                $sixDiceDefinition = new Definition(SixSidedDice::class);
+                $sixDiceDefinition->addTag('app.rollable');
+                $container->setDefinition('app.dice.six', $sixDiceDefinition);
 
-        $twelveDiceDefinition = new Definition(TwelveSidedDice::class);
-        $twelveDiceDefinition->addTag('app.rollable');
-        $container->setDefinition('app.dice.twelve', $sixDiceDefinition);
+                $twelveDiceDefinition = new Definition(TwelveSidedDice::class);
+                $twelveDiceDefinition->addTag('app.rollable');
+                $container->setDefinition('app.dice.twelve', $sixDiceDefinition);
 
-        $diceControllerDefinition = new Definition(DiceController::class);
-        $diceControllerDefinition->setArguments(array(new Reference('app.dice.six')));
-        $diceControllerDefinition->addMethodCall('setContainer', array(new Reference('service_container')));
-        $container->setDefinition('app.dice_controller', $diceControllerDefinition);
+                $diceControllerDefinition = new Definition(DiceController::class);
+                $diceControllerDefinition->setArguments(array(new Reference('app.dice.six')));
+                $diceControllerDefinition->addMethodCall('setContainer', array(new Reference('service_container')));
+                $container->setDefinition('app.dice_controller', $diceControllerDefinition);
+                break;
+            case "yml":
+                $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+                $loader->load('services.yml');
+                break;
+        }
     }
 }
